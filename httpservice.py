79a1ce6a7e99576980540a35be05efbcc2cdf2b8
SlAltum <Peter_Qiu1997@163.com>
2023-04-16 17:26:44 +0800

    更新了部分页面

--- httpservice.py
+++ httpservice.py
@@ -42,6 +42,35 @@ def get_logo_bg():
 @app.route("/background/apocalypse")
 def bg_apocalypse():
     return render_template("index.html",content=get_content("background/apocalypse.html"))
+# === post-apocalypse-life === #
+@app.route("/post-apocalypse-life/favicon.ico")
+def get_favicon_pal():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# === worldinfo === #
+@app.route("/worldinfo/favicon.ico")
+def get_favicon_worldinfo():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route("/worldinfo/the-world")
+def wd_info_theworld():
+    return render_template("index.html",content=get_content("/worldinfo/the-world.html"))
+# === game-concept === #
+@app.route("/game-concept/favicon.ico")
+def get_favicon_gc():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# === available-information === #
+@app.route("/available-information/favicon.ico")
+def get_favicon_ai():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# === about-us === #
+@app.route("/about-us/favicon.ico")
+def get_favicon_au():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.run(host="localhost",port=5000,debug=False)
